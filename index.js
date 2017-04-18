0a87f17b1a37b11c46f431e811cd47436fe6e085
Liah Wallace <liahwallace1@gmail.com>
2017-04-18 20:05:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-// function getFirstSelector(selector) {
-//   return document.querySelector(selector)
-// }
-//
-// function nestedTarget(className, idName) {
-//   return document.querySelector(idName.className)
-// }
-//
-// function increaseRankBy(n) {
-//   var ranks = document.querySelectorAll('.ranked-list li')
-//   for (let i = 0, l = ranks.length; i < l; i++) {
-//     parseInt('ranks[i].innerHTML', 10) += n
-//   }
-// }
-//
-// function deepestChild() {}
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0, l = ranks.length; i < l; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  let currentNode = document.getElementById('grand-node')
+  let nextNode = currentNode.children[0]
+
+  while (nextNode) {
+    currentNode = nextNode
+    nextNode = currentNode.children[0]
+  }
+  return currentNode
+}
