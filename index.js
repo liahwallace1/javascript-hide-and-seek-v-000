049d1ff5e2ef81692fedca28bdc5823562fea219
Liah Wallace <liahwallace1@gmail.com>
2017-04-18 18:32:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+// function getFirstSelector(selector) {
+//   return document.querySelector(selector)
+// }
+//
+// function nestedTarget(className, idName) {
+//   return document.querySelector(idName.className)
+// }
+//
+// function increaseRankBy(n) {
+//   var ranks = document.querySelectorAll('.ranked-list li')
+//   for (let i = 0, l = ranks.length; i < l; i++) {
+//     parseInt('ranks[i].innerHTML', 10) += n
+//   }
+// }
+//
+// function deepestChild() {}
